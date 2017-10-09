ddf7bfb26d6a09b8694c2a7875e65bf1b9ad7d1b
rajarshi8055 <rajarshichowdary@gmail.com>
2017-10-09 11:10:48 -0500

    cart after relativepath

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,7 +1,7 @@
 import {CartComponent} from './cart/cart.component';
 import {NavBarComponent} from './navbar/navbar.component';
 import {ProductComponent} from './product/product.component';
-import {ProductDetailComponent} from 'C:/Users/rajarshi/angular/LargeScaleEcommerceSite/src/app/components/product-detail/product-detail.component';
+import {ProductDetailComponent} from './../components/product-detail/product-detail.component';
 
 export const COMPONENTS = [
     CartComponent,
