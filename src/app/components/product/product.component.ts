147026ac9285f468a673d678af0ce77b0c88d56e
rajarshi8055 <rajarshichowdary@gmail.com>
2017-10-13 15:49:14 -0500

    cart error fixed

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -5,6 +5,7 @@ import {MdSnackBar} from '@angular/material';
 
 import { Product } from '../../models/Product';
 import { ProductService } from '../../services/product.service';
+import { RestuarantService } from '../../services/restuarant.service';
 import { CartAction } from '../../store/actions/cart.actions';
 
 @Component({
@@ -17,12 +18,14 @@ export class ProductComponent {
   
   products:Product[];
   quantity: number;
+  backgroundImg = "https://ak.picdn.net/assets/cms/97e1dd3f8a3ecb81356fe754a1a113f31b6dbfd4-stock-photo-photo-of-a-common-kingfisher-alcedo-atthis-adult-male-perched-on-a-lichen-covered-branch-107647640.jpg";
+
   
   CartMessage = "Successfully added to the Cart";
 
   // Angular will know to supply an instance of the ProductService & Router when it creates a new ProductComponent
   // Because they are injected in the constructor
-  constructor (private productService:ProductService, private router:Router, private cartStore: CartAction,public snackBar: MdSnackBar) {
+  constructor (private productService:RestuarantService, private router:Router, private cartStore: CartAction,public snackBar: MdSnackBar) {
   
   }
 
@@ -46,16 +49,18 @@ export class ProductComponent {
   }
 
   // When add to cart button is clicked
-  addToCart(product) {
+  addToCart(product,quan) {
     
+    this.quantity = parseInt(quan);
     // this.productService.addToCart(product)
     console.log(this.quantity)
-    this.cartStore.addToCart(product, this.quantity || 1);
+    this.cartStore.addToCart(product, this.quantity|| 1);
      this.openSnackBar(product.name,this.CartMessage);
   }
 
   getProductData() {     
-     this.productService.getProducts().then(products => this.products = products)
+     this.productService.getRestuarant().subscribe(products => this.products = products);
+     console.log(this.products);
   }
 
   ngOnInit() {
